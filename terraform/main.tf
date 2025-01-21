c542de50c19a5dafc389897bbcd972aad43db7e5
bodhiforge <bodhiyang.ca@gmail.com>
2025-01-20 16:32:27 -0800

    test v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,25 @@ resource "aws_ecs_cluster" "weather_cluster" {
 }
 
 resource "aws_security_group" "ecs_security_group" {
-  vpc_id = aws_vpc.main.id
+  name_prefix = "ecs-task-sg-"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "ecs-security-group"
+    Name = "ecs-task-security-group"
   }
 }
 
@@ -24,6 +40,12 @@ resource "aws_ecs_service" "frontend_service" {
     subnets          = aws_subnet.public_subnet[*].id
     security_groups  = [aws_security_group.ecs_security_group.id]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+    container_name   = "frontend-container"
+    container_port   = 3000
   }
 
   tags = {
@@ -44,6 +66,12 @@ resource "aws_ecs_service" "backend_service" {
     assign_public_ip = true
   }
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+    container_name   = "backend-container"
+    container_port   = 4000
+  }
+  
   tags = {
     Name = "backend-service"
   }
@@ -64,15 +92,6 @@ resource "aws_security_group_rule" "allow_http_backend" {
   to_port     = 4000
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ecs_security_group.id
-}
-
-resource "aws_security_group_rule" "allow_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ecs_security_group.id
 }
 
@@ -131,3 +150,112 @@ resource "aws_subnet" "public_subnet" {
 }
 
 data "aws_availability_zones" "available" {}
+
+resource "aws_security_group" "alb_security_group" {
+  name_prefix = "alb-sg-"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app_load_balancer" {
+  name               = "weather-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = aws_subnet.public_subnet.*.id
+}
+
+resource "aws_lb_target_group" "frontend_tg" {
+  name     = "frontend-target-group"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "backend-target-group"
+  port     = 4000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Not Found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend_rule" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["/frontend/*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "backend_rule" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority     = 200
+
+  condition {
+    path_pattern {
+      values = ["/backend/*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
